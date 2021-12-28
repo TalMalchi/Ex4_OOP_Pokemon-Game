9d79f6c8d154c05367381e7e0605a36776a43366
Daniel Appel <appel.daniel1998@gmail.com>
2021-12-28 17:21:13 +0200

    Changes by Tal

--- src/Agent.py
+++ src/Agent.py
@@ -1,13 +1,17 @@
+import json
+
+
 class Agent:
 
     def __init__(self, id, value, src, dest, speed, pos):
         self.id = id
-        self.value = value #how many did the agent eat till now
+        self.value = value  # how many did the agent eat till now
         self.dest = dest
         self.src = src
         self.speed = speed
         self.pos = pos
-        self.path = {}
+        self.path = []
+        self.all_agent = []
 
     def getId(self):
         """get the id of the agent"""
@@ -56,3 +60,41 @@ class Agent:
 
     def get_current_edge(self):
         """retur the edge that the agent is currently on"""
+
+    def loadAgent(self, file_name) -> bool:
+        try:
+            with open(file_name) as f:
+                data = f.read()
+            agent = json.loads(data)
+            for i in agent['Agents']:
+                self.all_agent.append(Agent(i['id'], i['value'],i['src'],i['dest'], i['speed'], i['pos']))
+            # self.id = agent['id']
+            # self.value = agent['value']
+            # self.src = agent['src']
+            # self.dest = agent['dest']
+            # self.speed = agent['speed']
+            # self.pos = agent['pos']
+
+        except IOError as e:
+            return False
+        return True
+
+    """
+
+                "Agents":[
+                    {
+                        "Agent":
+                        {
+                            "id":0,
+                            "value":0.0,
+                            "src":0,
+                            "dest":1,
+                            "speed":1.0,
+                            "pos":"35.18753053591606,32.10378225882353,0.0"
+                        }
+                    }
+                ]
+
+            """
+
+
