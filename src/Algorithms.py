da0cf305ce18d52ec7280d34a153ab570c0e9742
Daniel Appel <appel.daniel1998@gmail.com>
2022-01-02 22:17:22 +0200

    Changes by Tal

--- src/Algorithms.py
+++ src/Algorithms.py
@@ -8,15 +8,17 @@ from src.client import Client
 def assignAgentSrcNodes(numOfAgents: int, client: Client, pokLst: list, graph: nx.DiGraph) -> int:
     """Function to assign the agents their initial positions. returns the number of agents which have a pokemon to
     currently go to (if there are more agents than pokemons, not all agents might have a destination"""
+
     if numOfAgents == len(pokLst):
+        # if the amount of agents is equals to the pokemon amount, we will put each agent next to each pokemon
         for i in range(numOfAgents):
             client.add_agent("{\"id\":" + str(pokLst[i].node_src) + "}")
             return numOfAgents
-    elif numOfAgents < len(pokLst):
+    elif numOfAgents < len(pokLst):  # else, we will put the agent next to the pokemon with the highest value
         for i in range(numOfAgents):
             client.add_agent("{\"id\":" + str(pokLst[i].node_src) + "}")
         return numOfAgents
-    else:  # numOfAgents > len(pokLst)
+    else:  # numOfAgents > len(pokLst) - else, we will set random position to each agent
         for i in range(len(pokLst)):
             client.add_agent("{\"id\":" + str(pokLst[i].node_src) + "}")
         for i in range(numOfAgents - len(pokLst)):
@@ -26,3 +28,5 @@ def assignAgentSrcNodes(numOfAgents: int, client: Client, pokLst: list, graph: n
                 rand = random.randint(len(graph))
             client.add_agent("{\"id\":" + str(rand) + "}")
         return len(pokLst)
+
+
